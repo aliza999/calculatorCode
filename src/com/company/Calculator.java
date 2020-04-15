ca2e0fd4774ffabd815a563a5c17358aec0ee4d4
unknown <naila.shah777@gmail.com>
2020-04-15 06:34:40 +0530

    code1 Calculator

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,68 @@
+
+    package com.company;
+    import java.util.Scanner;
+    public class Calculator
+    {
+        static int result = 0;
+
+        public static void main(String[] args) {     //int result=0;
+            int opr1;
+            int opr2;
+            int choice;
+            Scanner s = new Scanner(System.in);
+
+            do {
+                System.out.println("\n 0 for exit");
+                System.out.print("\n 1:add");
+                System.out.print("\n 2:multiplication");
+                System.out.print("\n 3:divide");
+                System.out.print("\n 4:subtract");
+                System.out.print("\n Enter your choice");
+                choice = s.nextInt();
+                if (choice != 0) {
+
+                    switch (choice) {
+
+                        case 1: {
+                            opr1 = s.nextInt();
+                            opr2 = s.nextInt();
+                            result = opr1 + opr2;
+                            break;
+                        }
+                        case 2: {
+
+                            opr1 = s.nextInt();
+                            opr2 = s.nextInt();
+                            result = opr1 * opr2;
+                            break;
+                        }
+                        case 3: {
+
+                            opr1 = s.nextInt();
+                            opr2 = s.nextInt();
+                            if (opr2 != 0) {
+                                result = opr1 / opr2;
+                                break;
+                            }
+                            System.out.println("division not possible");
+                            break;
+                        }
+                        case 4: {
+                            opr1 = s.nextInt();
+                            opr2 = s.nextInt();
+
+
+                            result = opr1 - opr2;
+                            break;
+                        }
+                        default:
+                            System.out.println("no match found");
+                            break;
+                    }
+                    System.out.print("\n result is = " + result);
+                }
+            }
+            while (choice != 0);
+            // write your code here
+        }
+    }
